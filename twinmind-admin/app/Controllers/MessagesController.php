4ead8a55bbfd2ae5599c1e15c1274634a37bbfc6
yavuzslmmsml <yvz.slm6161@gmail.com>
2025-05-21 17:51:58 +0300

    carsamba-yavuz

--- twinmind-admin/app/Controllers/MessagesController.php
+++ twinmind-admin/app/Controllers/MessagesController.php
@@ -23,10 +23,10 @@ class MessagesController {
             'ProfileDetails' => $test
         ]);
     }
-     public function systemMessages() {
-         global $conn;
+    public function systemMessages() {
+        global $conn;
 
-         $query = "SELECT * FROM system_messages";
+        $query = "SELECT `id`, `recipient_role`, `title`, `message`, `sent_at` FROM system_messages";
 
         $result = mysqli_query($conn, $query);
 
@@ -35,12 +35,9 @@ class MessagesController {
             'Title' => 'All Courses',
             'Result' => $result
         ]);
-        
-    
-
-}
+    }
     public function AddSystemMessage() {
-         global $conn;
+        global $conn;
         $Errors = [];
 
         if (!isset($_POST['recipient_role']) || empty($_POST['recipient_role'])) {
@@ -55,27 +52,38 @@ class MessagesController {
             $Errors['message'][] = 'Message field is required';
         }
 
-       // Prepare the data for insertion
-       
+        // Prepare the data for insertion
+
+
         $recipient_role = mysqli_real_escape_string($conn, $_POST['recipient_role']);
- 
-        if (!in_array($recipient_role, [1, 2, 3, 4])) {
-            $Errors['recipient_role'][] = 'an error occurred';
-        }
         $title = mysqli_real_escape_string($conn, $_POST['title']);
         $message = mysqli_real_escape_string($conn, $_POST['message']);
 
+        if ($recipient_role == 0) {
+            $sql = "SELECT role_id FROM roles";
+            $result = mysqli_query($conn, $sql);
 
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $role_id = $row["role_id"];
 
-        $sql = "INSERT INTO system_messages (recipient_role, title, message, sent_at) 
+                    $insert = "INSERT INTO system_messages (recipient_role, title, message, sent_at)
+                       VALUES ('$role_id', '$title', '$message', NOW())";
+                    mysqli_query($conn, $insert);
+                }
+                exit(json_encode(['status' => true, 'message' => 'Message successfully sent!"' . $recipient_role . '"', 'redirect' => 'messages/systemMessages']));
+            } else {
+                exit(json_encode(['status' => false, 'errors' => ['general' => ['systemMessage failed. Please try again.']]]));
+            }
+        } else {
+            $sql = "INSERT INTO system_messages (recipient_role, title, message, sent_at) 
                 VALUES ('$recipient_role', '$title', '$message', NOW())";
 
-        if (mysqli_query($conn, $sql)) {
-            exit(json_encode(['status' => true, 'message' => 'Message successfully sent!', 'redirect' => 'messages/systemMessages']));
-        } else {
-            exit(json_encode(['status' => false, 'errors' => ['general' => ['systemMEssage failed. Please try again.']]]));
+            if (mysqli_query($conn, $sql)) {
+                exit(json_encode(['status' => true, 'message' => 'Message successfully sent!"' . $recipient_role . '"', 'redirect' => 'messages/systemMessages']));
+            } else {
+                exit(json_encode(['status' => false, 'errors' => ['general' => ['systemMessage failed. Please try again.']]]));
+            }
         }
-    
-
-}
+    }
 }
